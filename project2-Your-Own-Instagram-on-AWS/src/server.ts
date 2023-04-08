c4e04df367576422854352793f73d46f83940a80
Huy Đẹp Trai <huyxvd@gmail.com>
2023-04-08 12:42:09 +0700

    success upload & delete

--- project2-Your-Own-Instagram-on-AWS/src/server.ts
+++ project2-Your-Own-Instagram-on-AWS/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
 
@@ -9,7 +9,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -30,17 +30,34 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get("/", async (req, res) => {
+    const imageUrl = req.query.image_url;
+    // 1. validate the image_url query
+    if (imageUrl) {
+      // 2. call filterImageFromURL(image_url) to filter the image
+      const filteredpath = await filterImageFromURL(req.query.image_url);
+
+      console.log('filteredpath', filteredpath)
+
+      // 3. send the resulting file in the response
+      res.sendFile(filteredpath)
+
+      // 4. deletes any files on the server on finish of the response
+      setTimeout(async () => {
+        await deleteLocalFiles([filteredpath]);
+      }, 1000);
+    } else {
+      res.status(400).send("url empty");
+    }
+  });
+
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
