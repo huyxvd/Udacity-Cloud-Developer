5b191f8cdf918741a230c5b221a28e791160dc8d
Huy Đẹp Trai <huyxvd@gmail.com>
2023-05-25 08:59:44 +0700

    make the todo name atleast 11 character

--- project4-Serverless-Application/backend/src/lambda/http/createTodo.ts
+++ project4-Serverless-Application/backend/src/lambda/http/createTodo.ts
@@ -4,12 +4,28 @@ import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 import { createTodo } from '../../businessLogic/todos'
 import { createLogger } from '../../utils/logger'
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 
 const logger = createLogger('createTodoHandler')
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
+      
+    const parsedBody: CreateTodoRequest = JSON.parse(event.body)
+
+      if (parsedBody.name?.length <= 10) {
+        return {
+          statusCode: 400,
+          headers: {
+            'Access-Control-Allow-Origin': '*'
+          },
+          body: JSON.stringify({
+            error: 'name must be longer than 10 character'
+          })
+        }
+      }
+      
       const todoItem = await createTodo(event)
 
       logger.info('createdTodo', todoItem)
--- project4-Serverless-Application/backend/src/lambda/http/updateTodo.ts
+++ project4-Serverless-Application/backend/src/lambda/http/updateTodo.ts
@@ -4,12 +4,28 @@ import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 import { updateTodo } from '../../businessLogic/todos'
 import { createLogger } from '../../utils/logger'
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 
 const logger = createLogger('updateTodoHandler')
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
+      
+      const parsedBody: UpdateTodoRequest = JSON.parse(event.body)
+      
+      if (parsedBody.name?.length <= 10) {
+        return {
+          statusCode: 400,
+          headers: {
+            'Access-Control-Allow-Origin': '*'
+          },
+          body: JSON.stringify({
+            error: 'name must be longer than 10 character'
+          })
+        }
+      }
+
       const todoId = event.pathParameters.todoId
 
       if (!event.body || !todoId) {
