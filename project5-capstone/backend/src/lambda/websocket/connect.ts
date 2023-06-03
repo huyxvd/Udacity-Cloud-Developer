ebc3fbda73abec8edb7467d8622c24f5ee982f29
Huy Dep Trai <huyxvd@gmail.com>
2023-06-03 10:17:29 +0700

    success complete proj5

---
+++ project5-capstone/backend/src/lambda/websocket/connect.ts
@@ -0,0 +1,34 @@
+import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('createTodoHandler')
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const connectionsTable = process.env.CONNECTIONS_TABLE
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    logger.info('Websocket connect', event)
+
+    const connectionId = event.requestContext.connectionId
+    const timestamp = new Date().toISOString()
+
+    const item = {
+        id: connectionId,
+        timestamp
+    }
+
+    logger.info('Storing item', item)
+
+    await docClient.put({
+        TableName: connectionsTable,
+        Item: item
+    }).promise()
+
+    return {
+        statusCode: 200,
+        body: ''
+    }
+}
